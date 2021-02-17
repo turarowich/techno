aa38d04e92cf80150641f2017da181f0d487f36b
Ormonali <kaarov8@gmail.com>
2021-02-17 16:18:53 +0600

    fixes

--- app/controllers/clientController.js
+++ app/controllers/clientController.js
@@ -4,6 +4,42 @@ const db = global.userConnection.useDb("loygift");
 const Client = db.model("Client");
 const Category = db.model("Category");
 
+exports.getClient = async function (req, res) {
+    let result = {
+        'status': 200,
+        'msg': 'Sending client'
+    }
+    try {
+        let client = await Client.findById(req.params.client)
+        result['client'] = client
+    } catch (error) {
+        result = {
+            'status': 500,
+            'msg': error.message
+        }
+    }
+
+    res.status(result.status).json(result);
+};
+
+exports.getClients = async function (req, res) {
+    let result = {
+        'status': 200,
+        'msg': 'Sending clients'
+    }
+    try {
+        let clients = await Client.find()
+        result['clients'] = clients
+    } catch (error) {
+        result = {
+            'status': 500,
+            'msg': error.message
+        }
+    }
+
+    res.status(result.status).json(result);
+};
+
 exports.addClient = async function(req, res) {
     let result = {
         'status': 200,
@@ -21,7 +57,7 @@ exports.addClient = async function(req, res) {
             address: req.body.address,
             category: category._id
         }).save();
-
+        result['client'] = client
     } catch (error) {
         result = {
             'status': 500,
@@ -29,5 +65,42 @@ exports.addClient = async function(req, res) {
         }
     }
     
+    res.status(result.status).json(result);
+};
+
+exports.updateClient = async function (req, res) {
+    let result = {
+        'status': 200,
+        'msg': 'Client updated'
+    }
+    try {
+        let query = {'_id': req.params.client}
+        let client = await Client.findOneAndUpdate(query, req.body)
+        result['client'] = client
+    } catch (error) {
+        result = {
+            'status': 500,
+            'msg': error.message
+        }
+    }
+
+    res.status(result.status).json(result);
+};
+
+exports.deleteClient = async function (req, res) {
+    let result = {
+        'status': 200,
+        'msg': 'Client deleted'
+    }
+    try {
+        let query = { '_id': req.params.client }
+        await Client.findByIdAndRemove(query)
+    } catch (error) {
+        result = {
+            'status': 500,
+            'msg': error.message
+        }
+    }
+
     res.status(result.status).json(result);
 };
--- routes/api.js
+++ routes/api.js
@@ -1,6 +1,10 @@
 var clientController = require('../app/controllers/clientController');
 module.exports = function (app, network_information) {
+    app.get('/getClient/:client', clientController.getClient);
+    app.get('/getClients', clientController.getClients);
     app.post('/addClient', clientController.addClient);
-
+    app.put('/updateClient/:client', clientController.updateClient);
+    app.delete('/deleteClient/:client', clientController.deleteClient);
+    
     return app;
 }
