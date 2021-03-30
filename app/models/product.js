f8e5e88029e8f07bbfd78b5f4b160fa80e9077f6
Ormonali <kaarov8@gmail.com>
2021-03-30 12:24:04 +0600

    fixes

--- app/models/product.js
+++ app/models/product.js
@@ -18,6 +18,14 @@ const productSchema = new Schema({
         type: String,
         required: false,
     },
+    imgArray: [{
+        type: String,
+        required: false,
+    }],
+    status: {
+        type: String,
+        required: false,
+    },
     secondary: {
         type: String,
         required: false,
