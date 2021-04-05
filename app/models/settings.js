a88f39f829ab13f8af08ea655d5cae7d17283ddf
Ormonali <kaarov8@gmail.com>
2021-04-05 16:17:18 +0600

    fixes

---
+++ app/models/settings.js
@@ -0,0 +1,66 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const settingsSchema = new Schema({
+    name: {
+        type: String,
+        required: [true, 'Company name required'],
+    },
+    lastName: {
+        type: String,
+        required: [true, 'LastName required'],
+    },
+    phone: {
+        type: String,
+        required: false,
+        default: ''
+    },
+    email: {
+        type: String,
+        required: true,
+        validate: {
+            validator: async function (email) {
+                const user = await this.constructor.findOne({ email });
+                if (user) {
+                    if (this.id === user.id) {
+                        return true;
+                    }
+                    return false;
+                }
+                return true;
+            },
+            message: props => 'The specified email address is already in use.'
+        }
+    },
+    country: {
+        type: String,
+        required: false,
+        default: ''
+    },
+    address: {
+        type: String,
+        required: false,
+        default: ''
+    },
+    description: {
+        type: Number,
+        required: true,
+        default: 0
+    },
+    category: {
+        type: Schema.Types.ObjectId,
+        ref: 'user'
+    },
+    createdAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+    updatedAt: {
+        type: Date,
+        required: true,
+        default: Date.now,
+    },
+})
+
+module.exports = mongoose.model('Settings', settingsSchema)
