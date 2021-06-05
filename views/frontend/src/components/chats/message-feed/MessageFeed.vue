b6382ee4423386ff41abd7c9d6ba3a23ee2564d0
Ormonali <kaarov8@gmail.com>
2021-06-05 17:11:32 +0600

    fixes

--- views/frontend/src/components/chats/message-feed/MessageFeed.vue
+++ views/frontend/src/components/chats/message-feed/MessageFeed.vue
@@ -33,7 +33,7 @@ name: "MessageFeed",
         },
         messages: {
             handler: function (msgs) {
-                this.socket.emit('read messages', {'client': this.contact._id, 'isIncoming': true})
+                this.socket.emit('read messages', {'client': this.contact._id, 'isIncoming': false})
                 this.filteredMessages = msgs.map(function(msg){ msg.new = false; return msg }).slice(Math.max(msgs.length - this.show, 1)).reduce((groups, msg) => {
                     const date = msg.createdAt.split('T')[0];
                     if (!groups[date]) {
