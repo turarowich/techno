fb50d0fc7d962e050107e8ad43ae70ef1cb53972
Ormonali <kaarov8@gmail.com>
2021-06-15 18:01:22 +0600

    fixes

--- app.js
+++ app.js
@@ -145,6 +145,7 @@ const io = require('socket.io')(httpsServer, {
     },
     path: '/socket.io',
 });
+io.sockets.setMaxListeners(0);
 require("./routes/socket.js")(io)
 
 httpServer.listen(config.port_http, () => {
