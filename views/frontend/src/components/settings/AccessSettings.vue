917df98d78811bd82fd098746bd5de489db75c2b
Bektemir1 <kudajberdievbektemir@gmail.com>
2021-06-17 13:01:33 +0600

    fixes

--- views/frontend/src/components/settings/AccessSettings.vue
+++ views/frontend/src/components/settings/AccessSettings.vue
@@ -4,22 +4,24 @@
   <div class="row">
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/Setting.svg">
-        <div>
-          <h4 class="settings-title">Access to company settings</h4>
-          <div class="radio-toolbar">
-            <div class="d-flex align-items-center mr-3">
-              <input type="radio" id="setting-view" name="company"  checked>
-              <label for="setting-view"></label>
-              <span class="male">View only</span>
-            </div>
-            <div class="d-flex align-items-center">
-              <input type="radio" id="setting-edit" name="company" >
-              <label for="setting-edit"></label>
-              <span class="male">Edit access</span>
-            </div>
-          </div>
-        </div>
+       <div class="d-flex align-items-center">
+         <img class="settings-icon" src="../../assets/icons/setting-icon/Setting.svg">
+         <div>
+           <h4 class="settings-title">Access to company settings</h4>
+           <div class="radio-toolbar">
+             <div class="d-flex align-items-center mr-3">
+               <input type="radio" id="setting-view" name="company"  checked>
+               <label for="setting-view"></label>
+               <span class="male">View only</span>
+             </div>
+             <div class="d-flex align-items-center">
+               <input type="radio" id="setting-edit" name="company" >
+               <label for="setting-edit"></label>
+               <span class="male">Edit access</span>
+             </div>
+           </div>
+         </div>
+       </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -30,37 +32,25 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/Plus.svg">
+     <div class="d-flex align-items-center">
+       <img class="settings-icon" src="../../assets/icons/setting-icon/Plus.svg">
 
-        <div>
-          <h4 class="settings-title">Access to order</h4>
-          <div class="radio-toolbar">
-            <div class="d-flex align-items-center mr-3">
-              <input type="radio" id="client-view" name="order">
-              <label for="client-view"></label>
-              <span class="male">View only</span>
-            </div>
-            <div class="d-flex align-items-center">
-              <input type="radio" id="client-edit" name="order">
-              <label for="client-edit"></label>
-              <span class="male">Edit access</span>
-            </div>
-          </div>
-        </div>
-
-        <label class="switch d-flex">
-          <input type="checkbox">
-          <span class="slider round"></span>
-        </label>
-
-      </div>
-    </div>
-    <div class="col-lg-4">
-      <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/Chart.svg">
-        <div>
-          <h4 class="settings-title analytics">Access to Analytics</h4>
-        </div>
+       <div>
+         <h4 class="settings-title">Access to order</h4>
+         <div class="radio-toolbar">
+           <div class="d-flex align-items-center mr-3">
+             <input type="radio" id="client-view" name="order">
+             <label for="client-view"></label>
+             <span class="male">View only</span>
+           </div>
+           <div class="d-flex align-items-center">
+             <input type="radio" id="client-edit" name="order">
+             <label for="client-edit"></label>
+             <span class="male">Edit access</span>
+           </div>
+         </div>
+       </div>
+     </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -71,23 +61,12 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/Document.svg">
-
-        <div>
-          <h4 class="settings-title">Access to the clients base</h4>
-          <div class="radio-toolbar">
-            <div class="d-flex align-items-center mr-3">
-              <input type="radio" id="order-view" name="client">
-              <label for="order-view"></label>
-              <span class="male">View only</span>
-            </div>
-            <div class="d-flex align-items-center">
-              <input type="radio" id="order-edit" name="client">
-              <label for="order-edit"></label>
-              <span class="male">Edit access</span>
-            </div>
-          </div>
-        </div>
+       <div class="d-flex align-items-center">
+         <img class="settings-icon" src="../../assets/icons/setting-icon/Chart.svg">
+         <div>
+           <h4 class="settings-title ">Access to Analytics</h4>
+         </div>
+       </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -98,23 +77,25 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/VolumeUp.svg">
+       <div class="d-flex align-items-center">
+         <img class="settings-icon" src="../../assets/icons/setting-icon/Document.svg">
 
-        <div>
-          <h4 class="settings-title">Access to news</h4>
-          <div class="radio-toolbar">
-            <div class="d-flex align-items-center mr-3">
-              <input type="radio" id="news-view" name="news" >
-              <label for="news-view"></label>
-              <span class="male">View only</span>
-            </div>
-            <div class="d-flex align-items-center">
-              <input type="radio" id="news-edit" name="news">
-              <label for="news-edit"></label>
-              <span class="male">Edit access</span>
-            </div>
-          </div>
-        </div>
+         <div>
+           <h4 class="settings-title">Access to the clients base</h4>
+           <div class="radio-toolbar">
+             <div class="d-flex align-items-center mr-3">
+               <input type="radio" id="order-view" name="client">
+               <label for="order-view"></label>
+               <span class="male">View only</span>
+             </div>
+             <div class="d-flex align-items-center">
+               <input type="radio" id="order-edit" name="client">
+               <label for="order-edit"></label>
+               <span class="male">Edit access</span>
+             </div>
+           </div>
+         </div>
+       </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -125,6 +106,35 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
+       <div class="d-flex align-items-center">
+         <img class="settings-icon" src="../../assets/icons/setting-icon/VolumeUp.svg">
+         <div>
+           <h4 class="settings-title">Access to news</h4>
+           <div class="radio-toolbar">
+             <div class="d-flex align-items-center mr-3">
+               <input type="radio" id="news-view" name="news" >
+               <label for="news-view"></label>
+               <span class="male">View only</span>
+             </div>
+             <div class="d-flex align-items-center">
+               <input type="radio" id="news-edit" name="news">
+               <label for="news-edit"></label>
+               <span class="male">Edit access</span>
+             </div>
+           </div>
+         </div>
+       </div>
+
+        <label class="switch d-flex">
+          <input type="checkbox">
+          <span class="slider round"></span>
+        </label>
+
+      </div>
+    </div>
+    <div class="col-lg-4">
+      <div class="settings-box d-flex align-items-center justify-content-between">
+      <div class="d-flex align-items-center">
         <img class="settings-icon" src="../../assets/icons/setting-icon/Discovery.svg">
 
         <div>
@@ -142,6 +152,7 @@
             </div>
           </div>
         </div>
+      </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -152,23 +163,25 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/MoreSquare.svg">
+       <div class="d-flex align-items-center">
+         <img class="settings-icon" src="../../assets/icons/setting-icon/MoreSquare.svg">
 
-        <div>
-          <h4 class="settings-title">Chat access</h4>
-          <div class="radio-toolbar">
-            <div class="d-flex align-items-center mr-3">
-              <input type="radio" id="analatycs-view" name="chat" >
-              <label for="analatycs-view"></label>
-              <span class="male">View only</span>
-            </div>
-            <div class="d-flex align-items-center">
-              <input type="radio" id="analatycs-edit" name="chat">
-              <label for="analatycs-edit"></label>
-              <span class="male">Edit access</span>
-            </div>
-          </div>
-        </div>
+         <div>
+           <h4 class="settings-title">Chat access</h4>
+           <div class="radio-toolbar">
+             <div class="d-flex align-items-center mr-3">
+               <input type="radio" id="analatycs-view" name="chat" >
+               <label for="analatycs-view"></label>
+               <span class="male">View only</span>
+             </div>
+             <div class="d-flex align-items-center">
+               <input type="radio" id="analatycs-edit" name="chat">
+               <label for="analatycs-edit"></label>
+               <span class="male">Edit access</span>
+             </div>
+           </div>
+         </div>
+       </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -179,6 +192,7 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
+      <div class="d-flex align-items-center">
         <img class="settings-icon" src="../../assets/icons/setting-icon/Game.svg">
 
         <div>
@@ -196,6 +210,7 @@
             </div>
           </div>
         </div>
+      </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -206,22 +221,24 @@
     </div>
     <div class="col-lg-4">
       <div class="settings-box d-flex align-items-center justify-content-between">
-        <img class="settings-icon" src="../../assets/icons/setting-icon/paper.svg">
-        <div>
-          <h4 class="settings-title">Access to operations</h4>
-          <div class="radio-toolbar">
-            <div class="d-flex align-items-center mr-3">
-              <input type="radio" id="operation-view" name="operation" >
-              <label for="operation-view"></label>
-              <span class="male">View only</span>
-            </div>
-            <div class="d-flex align-items-center">
-              <input type="radio" id="operation-edit" name="operation" >
-              <label for="operation-edit"></label>
-              <span class="male">Edit access</span>
-            </div>
-          </div>
-        </div>
+     <div class="d-flex align-items-center">
+       <img class="settings-icon" src="../../assets/icons/setting-icon/paper.svg">
+       <div>
+         <h4 class="settings-title">Access to operations</h4>
+         <div class="radio-toolbar">
+           <div class="d-flex align-items-center mr-3">
+             <input type="radio" id="operation-view" name="operation" >
+             <label for="operation-view"></label>
+             <span class="male">View only</span>
+           </div>
+           <div class="d-flex align-items-center">
+             <input type="radio" id="operation-edit" name="operation" >
+             <label for="operation-edit"></label>
+             <span class="male">Edit access</span>
+           </div>
+         </div>
+       </div>
+     </div>
 
         <label class="switch d-flex">
           <input type="checkbox">
@@ -315,6 +332,7 @@ export default {
 .settings-icon{
   width: 30px;
   height: 30px;
+  margin-right: 10px;
 }
 .settings-title{
   font-size: 16px;
