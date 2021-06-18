6c7d37b3405894a9e0654e072d0b24bd7fd5cddc
Ormonali <kaarov8@gmail.com>
2021-06-18 14:00:06 +0600

    fixes

--- app/controllers/productController.js
+++ app/controllers/productController.js
@@ -111,7 +111,7 @@ class ProductController{
                     }
                 }
             }    
-            product.save()
+            await product.save()
             result['object'] = product
         } catch (error) {
             result = sendError(error, lang)
